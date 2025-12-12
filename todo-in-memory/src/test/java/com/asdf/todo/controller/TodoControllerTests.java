441ab3b86dac6eb9fd3e781138e4a5616319b16d
E. K. Lee <wolf@teoal.net>
2025-12-12 23:48:11 +0900

    3장 todo-in-memory 실습 완료

--- todo-in-memory/src/test/java/com/asdf/todo/controller/TodoControllerTests.java
+++ todo-in-memory/src/test/java/com/asdf/todo/controller/TodoControllerTests.java
@@ -1,4 +1,123 @@
 package com.asdf.todo.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import com.asdf.todo.model.Todo;
+import com.asdf.todo.service.TodoService;
+import java.util.Collections;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+/*
+
+
+*/
+
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(TodoController.class)
 public class TodoControllerTests {
+
+    @Autowired private MockMvc mockMvc;
+
+    @MockitoBean
+    private TodoService todoService;
+
+
+    @Test
+    public void testGetTodoById() throws Exception {
+        Todo todo = new Todo();
+        todo.setId(1L);
+        todo.setTitle("Test Todo");
+
+        given(todoService.findById(1L)).willReturn(todo);
+
+        mockMvc.perform(get("/api/todos/v1/1")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.title").value("Test Todo"));
+    }
+
+    @Test
+    public void testGetAllTodos() throws Exception {
+        given(todoService.findAll()).willReturn(Collections.emptyList());
+
+        mockMvc.perform(get("/api/todos/v1").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+        given(todoService.findAll())
+                .willReturn(
+                        Collections.singletonList(
+                                // 여기 Description은 넣어준 적이 없는데? LomBok이 생성자에서 넣어주나?
+                                new Todo(1L, "Test Todo", "Description", false))
+                );
+
+        mockMvc.perform(get("/api/todos/v1").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(1L))
+                .andExpect(jsonPath("$[0].title").value("Test Todo"));
+    }
+
+    @Test
+    public void testCreateTodo() throws Exception {
+        Todo todo = new Todo();
+        todo.setId(1L);
+        todo.setTitle("New Todo");
+
+        given(todoService.save(any(Todo.class))).willReturn(todo);
+
+        mockMvc.perform(
+                        // 이건 POST라서 보내는 내용이 좀 복잡하구나...
+                        post("/api/todos/v1")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{\"title\": \"New Todo\"}"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.title").value("New Todo"));
+    }
+
+    @Test
+    public void testUpdateTodo() throws Exception {
+        Todo existingTodo = new Todo();
+        existingTodo.setId(1L);
+        existingTodo.setTitle("Existing Todo");
+
+        Todo updateTodo = new Todo();
+        updateTodo.setId(1L);
+        updateTodo.setTitle("Updated Todo");
+
+        given(todoService.findById(1L)).willReturn(existingTodo);
+        given(todoService.update(anyLong(), any(Todo.class))).willReturn(updateTodo);
+
+        mockMvc.perform(
+                        put("/api/todos/v1/1")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{\"title\": \"Updated Todo\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.title").value("Updated Todo")
+                );
+    }
+
+    @Test
+    public void testDeleteTodo() throws Exception {
+        Todo todo = new Todo();
+        todo.setId(1L);
+        todo.setTitle("Test Todo");
+
+        given(todoService.findById(1L)).willReturn(todo);
+
+        mockMvc.perform(delete("/api/todos/v1/1").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
 }
--- todo-in-memory/src/test/java/com/asdf/todo/service/TodoServiceTests.java
+++ todo-in-memory/src/test/java/com/asdf/todo/service/TodoServiceTests.java
@@ -1,4 +1,62 @@
 package com.asdf.todo.service;
 
+import com.asdf.todo.model.Todo;
+import com.asdf.todo.repository.TodoInMemoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
 public class TodoServiceTests {
+    @Autowired private TodoService todoService;
+
+    @BeforeEach
+    void setup() {
+        todoService = new TodoService(new TodoInMemoryRepository());
+        todoService.save(new Todo(null, "Test Todo 1", "Description 1", false));
+        todoService.save(new Todo(null, "Test Todo 2", "Description 2", false));
+    }
+
+    @Test
+    void testFindAll() {
+        List<Todo> todos = todoService.findAll();
+        assertThat(todos).hasSize(2);
+    }
+
+    @Test
+    void testSaveTodo() {
+        Todo todo = new Todo(null, "New Todo", "New Description", false);
+        todoService.save(todo);
+        assertThat(todoService.findAll()).hasSize(3);
+    }
+
+    @Test
+    void testFindById() {
+        Todo todo = todoService.findById(1L);
+        assertThat(todo).isNotNull();
+        assertThat(todo.getTitle()).isEqualTo("Test Todo 1");
+    }
+
+    @Test
+    void testUpdateTodo() {
+        Todo updatedTodo = new Todo(1L, "Updated Todo", "Updated Description", true);
+        todoService.update(1L, updatedTodo);
+        Todo todo = todoService.findById(1L);
+        assertThat(todo.getTitle()).isEqualTo(updatedTodo.getTitle());
+        assertThat(todo.getDescription()).isEqualTo(updatedTodo.getDescription());
+        assertThat(todo.isCompleted()).isEqualTo(updatedTodo.isCompleted());
+    }
+
+    @Test
+    void testDeleteTodo() {
+        todoService.delete(1L);
+        // @BeforeEach에서 각 테스트별로 실행 전에 2개를 만들어 넣으니까, 여기서는 한개...
+        assertThat(todoService.findAll()).hasSize(1);
+        assertThat(todoService.findById(1L)).isNull();
+    }
 }
