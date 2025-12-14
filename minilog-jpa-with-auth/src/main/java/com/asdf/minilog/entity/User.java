b1ad6414cf0dc1fcaceecad337e466fbabc367dc
E. K. Lee <wolf@teoal.net>
2025-12-15 02:03:58 +0900

    6장 minilog 인증 실습 중 1

--- minilog-jpa-with-auth/src/main/java/com/asdf/minilog/entity/User.java
+++ minilog-jpa-with-auth/src/main/java/com/asdf/minilog/entity/User.java
@@ -8,18 +8,22 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 public class User {
+
+    private static PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,6 +43,14 @@ public class User {
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(
+            name = "user_roles", joinColumns = @JoinColumn(name = "user_id")
+    )
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private Set<Role> roles;
+
     @OneToMany(
             mappedBy = "author",
             cascade = CascadeType.ALL,
@@ -46,4 +58,70 @@ public class User {
             fetch = FetchType.LAZY
     )
     private List<Article> articles;
+
+    public static UserBuilder builder() {
+        return new UserBuilder();
+    }
+
+    public void setPassword(String password) {
+        this.password = passwordEncoder.encode(password);
+    }
+
+    public static class  UserBuilder {
+        private Long id;
+        private String username;
+        private String password;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+        private Set<Role> roles;
+        private List<Article> articles;
+        private static PasswordEncoder passwordEncoder = User.passwordEncoder;
+
+        public UserBuilder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public UserBuilder username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public UserBuilder password(String password) {
+            this.password = passwordEncoder.encode(password);
+            return this;
+        }
+
+        public UserBuilder createdAt(LocalDateTime createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public UserBuilder updatedAt(LocalDateTime updatedAt) {
+            this.updatedAt = updatedAt;
+            return this;
+        }
+
+        public UserBuilder roles(Set<Role> roles) {
+            this.roles = roles;
+            return this;
+        }
+
+        public UserBuilder articles(List<Article> articles) {
+            this.articles = articles;
+            return this;
+        }
+
+        public User build() {
+            User user = new User();
+            user.id = this.id;
+            user.username = this.username;
+            user.password = this.password;
+            user.createdAt = this.createdAt;
+            user.updatedAt = this.updatedAt;
+            user.roles = this.roles;
+            user.articles = this.articles;
+            return user;
+        }
+    }
 }
