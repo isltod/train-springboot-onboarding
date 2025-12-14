e98b06e630493c4db447ef1801381e397c41c648
E. K. Lee <wolf@teoal.net>
2025-12-14 23:30:20 +0900

    5장 minilog 실습 중 2

--- minilog-jpa/src/main/java/com/asdf/minilog/entity/Follow.java
+++ minilog-jpa/src/main/java/com/asdf/minilog/entity/Follow.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDateTime;
 
 @Entity
@@ -25,7 +27,7 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@EntityListeners(EntityListeners.class)
+@EntityListeners(AuditingEntityListener.class)
 public class Follow {
 
     @Id
@@ -48,3 +50,27 @@ public class Follow {
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 }
+/*
+public class Follow {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "follower_id", nullable = false)
+  private User follower;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "followee_id", nullable = false)
+  private User followee;
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at", nullable = false)
+  private LocalDateTime updatedAt;
+}
+*/
